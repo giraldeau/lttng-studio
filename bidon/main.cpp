b57d2e423db02768d3b87f90c20409dc21dc2fc2
Francis Giraldeau <francis.giraldeau@gmail.com>
2013-10-18 17:08:53 -0400

    blah

--- bidon/main.cpp
+++ bidon/main.cpp
@@ -9,7 +9,7 @@ int g = 0;
 int main()
 {
     ucontext_t ucp;
-    register long long int r = 0;
+    register long long int r asm("r10") = 42;
     int i = 0;
     int buf[10];
     memset(buf, 0, sizeof(buf));
@@ -17,13 +17,19 @@ int main()
     cout << "global=" << g
          << " register=" << r
          << " stack=" << i
-         << " buf[0]=" << buf[0]
          << endl;
+
     g++; r++; i++;
     for (int x = 0; x < 10; x++)
-        buf[x] = g;
+        cout << buf[x] << endl;
+    for (int x = 0; x < 10; x++)
+        buf[x] = x + g + r + i;
+    for (int x = 0; x < 10; x++)
+        cout << buf[x] << endl;
     if (g < 3)
         setcontext(&ucp);
+    for (int x = 0; x < 10; x++)
+        cout << buf[x] << endl;
     cout << "Done" << endl;
     return 0;
 }
